bc6eb2769e096e7c52b40073f29c077de5d95cd2
Simion Agavriloaei <simion.agv@gmail.com>
2017-06-22 23:04:58 +0300

    Basic -r inclusion detection (needs testing)

--- pip_upgrader/requirements_detector.py
+++ pip_upgrader/requirements_detector.py
@@ -27,6 +27,7 @@ class RequirementsDetector(object):
                 self.filenames.append(argument)
             else:  # pragma: nocover
                 print('Invalid requirements file: {}'.format(argument))
+        self._check_inclusions_recursively()
 
     def autodetect_files(self):
         """ Attempt to detect requirements files in the current working directory """
@@ -41,8 +42,24 @@ class RequirementsDetector(object):
                 file_path = os.path.join('requirements', filename)
                 if self._is_valid_requirements_file(file_path):
                     self.filenames.append(file_path)
+        self._check_inclusions_recursively()
 
     @staticmethod
     def _is_valid_requirements_file(filename):
         extension_ok = filename.endswith('txt') or filename.endswith('pip')
         return extension_ok and os.path.isfile(filename) and mimetypes.guess_type(filename)[0] in ['text/plain', None]
+
+    def _check_inclusions_recursively(self):
+        for filename in self.filenames:
+            self._detect_inclusion(filename)
+
+    def _detect_inclusion(self, filename):
+        with open(filename) as fh:
+            for line in fh:
+                if line.strip().startswith('-r '):
+                    included_filename = line.split('-r ')[1].strip()
+                    included_filename = os.path.join(os.path.dirname(filename), included_filename)
+                    if self._is_valid_requirements_file(included_filename) and included_filename not in self.filenames:
+                        self.filenames.append(included_filename)
+                        # recursively, check if the included file contains other inclusions
+                        self._detect_inclusion(included_filename)
